6adb5cf7a96d9ae930f3f1db334b1f11c47fdb32
Stepan Andreev <stepan10andreev@gmail.com>
2023-06-07 16:31:37 +0300

    add IMage-next in Preview

--- src/components/CardList/Card/Preview/Preview.tsx
+++ src/components/CardList/Card/Preview/Preview.tsx
@@ -1,23 +1,26 @@
 import { FC } from 'react';
 import styles from './Peview.module.scss';
+import Image from 'next/image';
 
 interface IPreviewProps {
   previewImgUrl: string;
   media: null | {};
 }
 
+const imageStyle = {
+  width: '100%',
+  height: '100%',
+  "object-fit": 'cover',
+};
+
 export const Preview: FC<IPreviewProps> = ({previewImgUrl, media}) => {
   console.log(media)
   return (
     <div className={styles.preview}>
-      {media === null ? (
-        <img className={styles.previewImg} src={previewImgUrl} alt="previewImg" />
-
+      {((media === null) && (previewImgUrl != 'default')) ? (
+        <Image src={previewImgUrl} width={200} height={110} alt="preview image" style={imageStyle}/>
       ) : (
-        <img className={styles.previewImg} src='/media-1.jpg' alt="previewImg" />
-        // <div className={styles.mediaPost}>
-        //   Media Post
-        // </div>
+        <Image src='/media-1.jpg' width={200} height={110} alt="media-post" style={imageStyle}/>
       )}
     </div>
   );
