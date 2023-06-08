bca04d614980e7bb0efaa7b67624e3c5f91a413a
Stepan Andreev <stepan10andreev@gmail.com>
2023-06-08 15:37:09 +0300

    add get subredditData in PostPage and optimizate data

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -6,11 +6,11 @@ import { useRouter } from "next/router";
 import Image from 'next/image';
 import styles from './[id].module.scss';
 
-const props = ['id', 'title', 'thumbnail', 'permalink', 'author', 'score', 'num_comments', 'created', 'media']
+const postProps = ['title', 'thumbnail', 'permalink', 'author', 'media', 'subreddit_name_prefixed']
+const subredditProps = ['title', 'public_description', 'url', 'icon_img', 'banner_img', 'subscribers']
 
 export const getServerSideProps: GetServerSideProps = async ({ query, req, res }: GetServerSidePropsContext) => {
-  const token = getCookie('token', { req, res })
-  console.log(token)
+  const token = getCookie('token', { req, res });
 
   if (!token) return {
     props: {postsData: []},
@@ -19,47 +19,59 @@ export const getServerSideProps: GetServerSideProps = async ({ query, req, res }
     //   permanent: false,
     // },
   };
-  console.group(query.id)
+
   const {data} = await axios.get(`https://oauth.reddit.com/api/info.json?id=t3_${query.id}`, {
     headers: { Authorization: `bearer ${token}` },
   })
 
   const postData = data.data.children.map((item: { data: any }) => item.data)
-  // const optimizatedData = getOptimizatedData(postsData, props)
-  // console.log(postsData)
+  const optimizatedPostData = getOptimizatedData(postData, postProps)
+
+  const subreddit = await axios.get(`https://oauth.reddit.com/api/info.json?id=${postData[0].subreddit_id}`, {
+    headers: { Authorization: `bearer ${token}` },
+  })
+
+  const subredditData = subreddit.data.data.children.map((item: { data: any }) => item.data)
+  const optimizatedSubredditData = getOptimizatedData(subredditData, subredditProps)
+  console.log(subredditData)
+
   return {
-    props: {postData: postData},
+    props: {postData: optimizatedPostData[0], subredditData: optimizatedSubredditData[0]},
   }
 }
 export interface IPostPageProps {
-  postData: any[];
+  postData: any;
+  subredditData: any;
 }
 
-export const PostPage: NextPage<IPostPageProps>  = ({postData}) => {
+export const PostPage: NextPage<IPostPageProps>  = ({postData, subredditData}) => {
   const router = useRouter()
-  const { id } = router.query;
-  const postUrl = "https://www.reddit.com" + postData[0].permalink;
+  // const { id } = router.query;
+  const postUrl = "https://www.reddit.com" + postData.permalink;
+  const subredditUrl = "https://www.reddit.com" + subredditData.url;
+  console.log(subredditData)
   console.log(postData)
   return (
     <div className={styles.post}>
       <div className={styles.postWrapper}>
         <h1 className={styles.title}>
-          {postData[0].title}
+          {postData.title}
         </h1>
-        {postData[0].media != null ? (
-            <video className={styles.media} src={postData[0].media.reddit_video.scrubber_media_url} autoPlay></video>
+        {postData.media != null ? (
+            <video className={styles.media} src={postData.media.reddit_video.scrubber_media_url} autoPlay></video>
         ) : (
-          <Image src={postData[0].thumbnail} width={200} height={110} alt="preview image"/>
+          <Image src={postData.thumbnail} width={200} height={110} alt="preview image"/>
         )}
         <a href={postUrl}>
           Link to post on Reddit
         </a>
       </div>
       <div className={styles.subredditInfo}>
-          <h2>{postData[0].subreddit_name_prefixed}</h2>
-          <p>Description</p>
-          <p>subreddit_subscribers</p>
-          <a href="subreddit_name_prefixed">LINK</a>
+
+          <h2>{subredditData.title}</h2>
+          <small>{postData.subreddit_name_prefixed}</small>
+          <p>{subredditData.subscribers}</p>
+          <a href={subredditUrl}>LINK</a>
       </div>
     </div>
   )
